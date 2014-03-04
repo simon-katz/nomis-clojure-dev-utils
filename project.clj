aa4aeba468c4c4b930ea5d2c28c65124edc99bdd
Simon Katz <sk@nomistech.com>
2014-03-04 14:29:40 +0000

    Add a :dev profile.

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "None"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles
+  {:dev {:source-paths ["dev"]
+         :dependencies [[midje "1.6.2"]]
+         :plugins [[lein-midje "3.1.1"]]}})
